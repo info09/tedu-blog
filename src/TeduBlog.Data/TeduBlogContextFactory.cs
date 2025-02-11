29d356d1113424869e70af75f1c6b8dc36b4be78
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-02-11 11:25:27 +0700

    Migrate database

---
+++ src/TeduBlog.Data/TeduBlogContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace TeduBlog.Data
+{
+    public class TeduBlogContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
