354d632d62af9a1c2d41f2c08a9c2370b6a80305
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-02-12 16:41:21 +0700

    role management backend

---
+++ src/TeduBlog.Api/Controllers/AdminApi/RoleController.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeduBlog.Api.Filters;
+using TeduBlog.Core.Domain.Identity;
+using TeduBlog.Core.Models;
+using TeduBlog.Core.Models.System;
+using TeduBlog.Core.SeedWorks.Constants;
+
+namespace TeduBlog.Api.Controllers.AdminApi
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public RoleController(RoleManager<AppRole> roleManager, IMapper mapper)
+        {
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Authorize(Permissions.Roles.View)]
+        public async Task<ActionResult<List<RoleDto>>> GetAllRoles()
+        {
+            var model = await _mapper.ProjectTo<RoleDto>(_roleManager.Roles).ToListAsync();
+            return Ok(model);
+        }
+
+        [HttpGet("paging")]
+        [Authorize(Permissions.Roles.View)]
+        public async Task<ActionResult<PagedResult<RoleDto>>> GetRolesPagingAsync(string? keyword, int pageIndex, int pageSize)
+        {
+            var query = _roleManager.Roles;
+            query = !string.IsNullOrEmpty(keyword) ? query.Where(i => i.Name!.ToLower().Contains(keyword.ToLower()) || i.DisplayName.ToLower().Contains(keyword.ToLower())) : query;
+
+            var totalRow = await query.CountAsync();
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            var data = await _mapper.ProjectTo<RoleDto>(query).ToListAsync();
+            var pagination = new PagedResult<RoleDto>
+            {
+                Items = data,
+                RowCount = totalRow,
+                PageSize = pageSize,
+                CurrentPage = pageIndex
+            };
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Permissions.Roles.View)]
+        public async Task<ActionResult<RoleDto>> GetRoleById(Guid id)
+        {
+            var role = await _roleManager.FindByIdAsync(id.ToString());
+            if (role == null)
+                return NotFound();
+            return Ok(_mapper.Map<RoleDto>(role));
+        }
+
+
+        [HttpPost]
+        [ValidateModel]
+        [Authorize(Permissions.Roles.Create)]
+        public async Task<IActionResult> CreateRole([FromBody] CreateUpdateRoleRequest request)
+        {
+            await _roleManager.CreateAsync(new AppRole
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                DisplayName = request.DisplayName
+            });
+            return new OkResult();
+        }
+
+        [HttpPut("{id}")]
+        [ValidateModel]
+        [Authorize(Permissions.Roles.Edit)]
+        public async Task<IActionResult> UpdateRole(Guid id, [FromBody] CreateUpdateRoleRequest request)
+        {
+            var role = await _roleManager.FindByIdAsync(id.ToString());
+            if (role == null)
+                return NotFound();
+            role.Name = request.Name;
+            role.DisplayName = request.DisplayName;
+            await _roleManager.UpdateAsync(role);
+            return Ok();
+        }
+        [HttpDelete]
+        [Authorize(Permissions.Roles.Delete)]
+        public async Task<IActionResult> DeleteRoles([FromQuery] Guid[] ids)
+        {
+            foreach (var id in ids)
+            {
+                var role = await _roleManager.FindByIdAsync(id.ToString());
+                if (role == null)
+                    return NotFound();
+                await _roleManager.DeleteAsync(role);
+            }
+            return Ok();
+        }
+    }
+}
